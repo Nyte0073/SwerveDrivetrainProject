3a0d8a8d1e5aeca4b410039fa001a845285a2aaf
Nyte0073 <148829307+Nyte0073@users.noreply.github.com>
2026-01-23 23:19:59 -0500

    Committing latest code (January 23rd, 2026)

--- src/subsystems/driveables/ThreadBasedSwerveDrivetrain.java
+++ src/subsystems/driveables/ThreadBasedSwerveDrivetrain.java
@@ -3,14 +3,22 @@ package subsystems.driveables;
 import vectors.Vector;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
 
 public class ThreadBasedSwerveDrivetrain extends Swerve {
 
+    private final EnumMap<Vector.WheelType, Double> wheelPositions = new EnumMap<>(Vector.WheelType.class);
 
     public ThreadBasedSwerveDrivetrain(Supplier<Vector> vectorSupplier, Supplier<Boolean> robotProgramShouldContinue, Supplier<Double> originalPosSupplier) {
         super(vectorSupplier, robotProgramShouldContinue, originalPosSupplier);
+
+        wheelPositions.put(Vector.WheelType.FRONT_LEFT, 0.0);
+        wheelPositions.put(Vector.WheelType.FRONT_RIGHT, 0.0);
+        wheelPositions.put(Vector.WheelType.BACK_LEFT, 0.0);
+        wheelPositions.put(Vector.WheelType.BACK_RIGHT, 0.0);
     }
 
     @Override
@@ -18,14 +26,27 @@ public class ThreadBasedSwerveDrivetrain extends Swerve {
         List<Vector> translatedAndOrRotatedVectors = new ArrayList<>();
         if(rotating) {
             for(Vector.WheelType wheelType : Vector.WheelType.wheelTypes) {
-
+                Vector rotatedVector, translatedAndRotatedVector;
+                double x = driverVector.getX();
+                double y = driverVector.getY();
+                if(clockwise) {
+                    rotatedVector = Vector.rotate90DegreesClockwise.apply(x, y);
+                } else {
+                    rotatedVector = Vector.rotate90DegreesCounterClockwise.apply(x, y);
+                }
+                translatedAndRotatedVector = rotatedVector.plus(driverVector);
+                translatedAndOrRotatedVectors.add(translatedAndRotatedVector);
             }
         } else {
             double targetAngle = Math.toDegrees(Math.atan2(driverVector.getY(),
                     driverVector.getX())) - 90;
-            double normalizedHeading = normalizeHeading(targetAngle, originalPos);
-            double totalHeading = originalPos + normalizedHeading;
-            double reversedHeading = reverseHeading(originalPos, totalHeading, targetAngle);
+
+            for(Map.Entry<Vector.WheelType, Double> entry : wheelPositions.entrySet()) {
+                double normalizedHeading = normalizeHeading(targetAngle, originalPos + entry.getValue());
+                double totalHeading = entry.getValue() + normalizedHeading;
+                double reversedHeading = reverseHeading(originalPos, totalHeading, targetAngle);
+            }
+
         }
 
         return translatedAndOrRotatedVectors;
